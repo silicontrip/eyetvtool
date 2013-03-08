1e51f3d5b873ad2b4cfa65f5b0b19fb68a0bcbe3
Mark Heath (@utopia) <mjpeg0@silicontrip.net>
2013-03-08 16:13:00 +1100

    Initial Commit

---
+++ eyetvtool/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  eyetvtool
+//
+//  Created by Mark Heath on 8/03/13.
+//  Copyright (c) 2013 Mark Heath. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
