b4b386d90fb531984228c91f8b48f055d703152b
Mark Heath (@inertia) <mark@silicontrip.net>
2013-07-19 07:42:55 +1000

    Updated build script to make universal binary. And made universal binary.

--- eyetvtool/quickbuild.sh
+++ eyetvtool/quickbuild.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-gcc -o eyetvtool -framework Foundation -framework ApplicationServices  *.m
+gcc -O2 -Wall -force_cpusubtype_ALL -mmacosx-version-min=10.5 -arch i386 -arch ppc -o eyetvtool -framework Foundation -framework ApplicationServices  *.m
